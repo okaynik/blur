b9deb109beb0f5a8f2dfe0f9ac3d2f8f0d73db29
okaynik <okay.nik2@gmail.com>
2023-01-15 17:24:42 -0500

    basic auth0 authentication

--- blur-frontend/src/pages/Main.tsx
+++ blur-frontend/src/pages/Main.tsx
@@ -5,6 +5,7 @@ import "../styles/Main.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { useEffect, useState } from "react";
 import { useAuth0 } from "@auth0/auth0-react";
+import { getProtectedResource } from "../services/message.service";
 
 export interface Post {
   id: number;
@@ -29,6 +30,9 @@ export default function Main() {
   const [isEditing, setIsEditing] = useState(false);
   const [title, setTitle] = useState("");
   const [body, setBody] = useState("");
+  const [message, setMessage] = useState<string>("");
+
+  const { user, logout, isLoading, getAccessTokenSilently } = useAuth0();
 
   const handleEdit = () => {
     setIsEditing(!isEditing);
@@ -49,24 +53,49 @@ export default function Main() {
     setIsEditing(false);
   };
 
-  const { user, logout, isLoading } = useAuth0();
-  useEffect(() => {
-    // if (isLoading) return;
-    if (!user) {
-      logout({ returnTo: window.location.origin });
-    }
-  }, [isLoading]);
-
   const handleLogout = () => {
     logout({
       returnTo: window.location.origin,
     });
   };
 
+  useEffect(() => {
+    if (!user) {
+      logout({ returnTo: window.location.origin });
+    }
+  }, [isLoading]);
+
+  useEffect(() => {
+    let isMounted = true;
+
+    const getMessage = async () => {
+      const accessToken = await getAccessTokenSilently();
+      const { data, error } = await getProtectedResource(accessToken);
+
+      if (!isMounted) {
+        return;
+      }
+
+      if (data) {
+        setMessage(JSON.stringify(data, null, 2));
+      }
+
+      if (error) {
+        setMessage(JSON.stringify(error, null, 2));
+      }
+    };
+
+    getMessage();
+
+    return () => {
+      isMounted = false;
+    };
+  }, [getAccessTokenSilently]);
+
   return (
     <div>
       {/* <TopBar /> */}
-      {/* <p>Hello {user}</p> */}
+      <p>Hello {user?.nickname}</p>
       <div className="Header mt-3 ml-2">
         <h1> {isEditing ? "Create new post" : "Top Posts"}</h1>
         <button className="btn btn-dark" onClick={handleEdit}>
@@ -102,6 +131,8 @@ export default function Main() {
           </div>
         </div>
       )}
+      {message}
+
       {/* {!isEditing && <Posts />} */}
     </div>
   );
