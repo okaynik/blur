a8d3ce02ee4dc99dc4ea87213ac3374b2b07a158
Naron <naron.chen@richmond.edu>
2023-04-01 22:42:39 -0400

    user page

--- blur-frontend/src/pages/Post/PostView.tsx
+++ blur-frontend/src/pages/Post/PostView.tsx
@@ -6,6 +6,9 @@ import { Response } from "../../models/response";
 import Layout from "../Layout";
 import { getPost, getResponses } from "../../services/posts.service";
 import { useMakeRequest } from "../../services/useMakeRequest";
+import '../../styles/PostView.css'
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faChevronUp, faChevronDown } from "@fortawesome/free-solid-svg-icons";
 
 export default function PostView() {
   const [answer, setAnswer] = useState("");
@@ -13,6 +16,8 @@ export default function PostView() {
   const post = useMakeRequest<Post>(getPost, id as string);
   const responses = useMakeRequest<Response[]>(getResponses, id as string);
   const [update, setUpdate] = useState(false);
+
+  const [showAddResponse, setShowAddResponse] = useState(false);
 
   const submitAnswer = () => {
     fetch("http://localhost:9000/api/res/add", {
@@ -28,25 +33,67 @@ export default function PostView() {
     });
     setAnswer("");
     setUpdate(!update);
+    setShowAddResponse(false);
+
   };
 
   return (
     <Layout>
-      <h1>{post?.title}</h1>
-      <br />
-      {post?.author}
-      <br />
-      {post?.body}
-      <br />
-      <h1>Responses</h1>
-      {responses?.map((response) => (
-        <div key={response.id}>
-          {response.author}
-          <br />
-          {response.body}
-          <br />
+      <div className="container">
+        <div className="post-view">
+          <h1>{post?.title}</h1>
+          <div className="post-details">
+            <p className="post-author"> {post?.author}</p>
+            <p className="post-body">{post?.body}</p>
+          </div>
+          <button onClick={() => setShowAddResponse(!showAddResponse)} className="add-response-button">
+              {showAddResponse? "Cancel" : "Add a response" }
+          </button>
+
         </div>
-      ))}
+        
+        <div className="add-response">
+          {showAddResponse && (
+            <>
+              <textarea
+                value={answer}
+                onChange={(e) => setAnswer(e.target.value)}
+                onFocus={(e) => e.target.style.outline = 'none'}
+                onBlur={(e) => e.target.style.outline = ''}
+              ></textarea>
+              <div className="response-button-group">
+                <button onClick={() => setShowAddResponse(false)}>Cancel</button>
+                <button onClick={submitAnswer}>Submit</button>
+              </div>
+            </>
+          )}
+        </div>
+
+
+        <div className="responses">
+          <h3>Responses</h3>
+          {responses?.map((response) => (
+            <div key={response.id} className="response">
+              <div className="response-details">
+                <p className="response-author"> {response.author}</p>
+                <p className="response-body">{response.body}</p>
+              </div>
+              <div className="response-meta">
+
+              <button className="upvote" >
+                <FontAwesomeIcon icon={faChevronUp} color="" />
+                {response.likes}
+              </button>
+              <button className="downvote" >
+                <FontAwesomeIcon icon={faChevronDown} color="" />
+              </button>
+                <p className="response-time">{response.time}</p>
+              </div>
+
+            </div>
+          ))}
+        </div>
+      </div>
     </Layout>
   );
 }
