a2cf21a06194e0a39fe7c7371349c2b26dd9b780
Naron <naron.chen@richmond.edu>
2023-04-08 17:19:16 -0400

    likes api

--- back/src/services/likes.service.ts
+++ back/src/services/likes.service.ts
@@ -8,12 +8,52 @@ async function add(
   nickname: string,
   vote: Vote
 ): Promise<void> {
-  return post.increment("likes", {
-    by: vote === "up" ? 1 : -1,
+
+  const postInstance = await post.findByPk(postId);
+  const [existingLike, created] = await like.findOrCreate({
     where: {
-      id: postId,
+      postId: postId,
+      nickname: nickname,
+    },
+    defaults: {
+      vote: vote,
     },
   });
+
+  if (created){
+    // user havent liked or disliked the post yet
+    await postInstance.increment("likes", {
+      by: vote === "up" ? 1 : -1,
+    });
+
+  } else {
+
+    // user has already liked or disliked the post
+    console.log('Row already exists:', existingLike.toJSON());
+    if (existingLike.vote === vote) {
+
+      // user is trying to like or dislike the post again, so we remove the like or dislike
+      await postInstance.increment("likes", {
+        by: vote === "up" ? -1 : 1,
+      });
+
+      await existingLike.destroy();
+
+    }
+
+    else {
+      // user is trying to change his like or dislike
+      await postInstance.increment("likes", {
+        by: vote === "up" ? 2 : -2,
+      });
+
+      await existingLike.update({
+        vote: vote,
+      });
+    }
+
+  }
+
 }
 
 export default {
